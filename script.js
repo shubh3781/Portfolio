6a5743c27bb3c5d2fc86cf52971fcb306d68a6ae
Shubh3781 <shubhpatel3781@gamil.com>
2024-08-26 20:51:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Alert on button click
+function showAlert() {
+    alert('Hello, this is a simple alert!');
+}
+
+// Add event listener to a button (if you add one later)
+document.querySelector('button').addEventListener('click', showAlert);
